d2b13a5ecba1373636293683ea89cd11d9fd5e01
Julian Della Bosca <julian.dellabosca@gmail.com>
2025-12-05 14:48:17 +1100

    debug: add client base url to discovery

--- api/src/functions/halaxy-api-discovery.ts
+++ api/src/functions/halaxy-api-discovery.ts
@@ -139,6 +139,7 @@ async function halaxyApiDiscoveryHandler(
       headers,
       jsonBody: {
         baseUrl,
+        clientBaseUrl: client['config'].apiBaseUrl,  // What the client uses
         envVar: process.env.HALAXY_API_URL || 'NOT SET (using default)',
         summary: {
           total: results.length,
